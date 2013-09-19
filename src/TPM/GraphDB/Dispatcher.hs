57d8feba8b10090dfa3e7c69f16a26919125a457
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-09-19 22:51:44 +0400

    FairRWLock-based Dispatcher

---
+++ src/TPM/GraphDB/Dispatcher.hs
@@ -0,0 +1,31 @@
+module TPM.GraphDB.Dispatcher (Dispatcher, new, runWrite, runRead) where
+
+import TPM.Prelude
+import qualified Control.Concurrent.FairRWLock as RWLock
+
+
+
+-- | 
+-- Controls the concurrency with the following rules:
+-- 
+-- * It ensures that when a \"write\"-action executes, no other action executes
+-- concurrently.
+-- 
+-- * It ensures that no \"write\"-actions get executed while a \"read\"-action 
+-- executes.
+-- 
+data Dispatcher = Dispatcher RWLock.RWLock
+
+new :: IO Dispatcher
+new = Dispatcher <$> RWLock.new
+
+-- |
+runWrite :: Dispatcher -> IO a -> IO a
+runWrite (Dispatcher lock) io = RWLock.withWrite lock io
+
+-- |
+runRead :: Dispatcher -> IO a -> IO a
+runRead (Dispatcher lock) io = RWLock.withRead lock io
+
+
+
