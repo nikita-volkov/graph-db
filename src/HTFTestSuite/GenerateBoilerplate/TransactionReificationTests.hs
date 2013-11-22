e5f3696b19bb5c520f0f21ef8b0832c39613edfc
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-11-22 21:48:20 +0400

    Isolate transaction functions reification bug in tests

--- src/HTFTestSuite/GenerateBoilerplate/TransactionReificationTests.hs
+++ src/HTFTestSuite/GenerateBoilerplate/TransactionReificationTests.hs
@@ -10,16 +10,42 @@ import qualified GraphDB.API as API
 
 
 type Read r = forall s. API.Read Char s r
+type Read' s r = API.Read Char s r
 type Write r = forall s. API.Write Char s r
+type Write' s r = API.Write Char s r
 
+transactionFunction1 :: Read' s Int
+transactionFunction1 = undefined
+
+transactionFunction2 :: Char -> Read' s Int
+transactionFunction2 = undefined
+
+
+test_reifyLocalTransactionFunctions = do
+  assertElem "transactionFunction2" functionNames
+  assertElem "transactionFunction1" functionNames
+  where
+    functionNames :: [String]
+    functionNames = read $(
+        reifyLocalTransactionFunctions (ConT ''Char) >>=
+        stringE . show . map (\(n, _, _ , _) -> show n)
+      )
 
 test_isWrite = do
   assertBool $ read $(
       reifyTransaction (ConT ''Write `AppT` ConT ''Int) >>=
       stringE . show . (== Just True) . fmap (\(a, _, _) -> a)
     )
+  assertBool $ read $(
+      reifyTransaction (ConT ''Write' `AppT` ConT ''Int) >>=
+      stringE . show . (== Just True) . fmap (\(a, _, _) -> a)
+    )
   assertBool $ not $ read $(
       reifyTransaction (ConT ''Read `AppT` ConT ''Int) >>=
+      stringE . show . (== Just True) . fmap (\(a, _, _) -> a)
+    )
+  assertBool $ not $ read $(
+      reifyTransaction (ConT ''Read' `AppT` ConT ''Int) >>=
       stringE . show . (== Just True) . fmap (\(a, _, _) -> a)
     )
 
@@ -34,4 +60,10 @@ test_readSynExpansion = do
       stringE . show . isJust =<< 
       reifyTransaction (ConT ''Read `AppT` ConT ''Int)
     )
+
+
+assertElem item list =
+  assertBoolVerbose 
+    ("Expected item '" ++ item ++ "' to be in " ++ show list)
+    (item `elem` list)
 
