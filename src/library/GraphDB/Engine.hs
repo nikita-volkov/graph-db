0ba60082c27e5ca5f904cc9eed1092566059a52a
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-02-08 00:33:23 +0400

    Move Server to Engine

--- src/library/GraphDB/Engine.hs
+++ src/library/GraphDB/Engine.hs
@@ -38,6 +38,12 @@ module GraphDB.Engine
     TagEvent(..),
     TagEventResult(..),
     FinalTransaction(..),
+
+    -- * Server
+    ServerMode(..), 
+    Server, 
+    shutdownServer, 
+    startServer,
   )
   where
 
@@ -437,3 +443,37 @@ getStats = do
   liftIO $ Node.getStats tn
 
 
+
+--------------------------------------------------------------------------------
+
+-- | 
+-- The settings of server.
+data ServerMode = 
+  -- | 
+  -- A port to run the server on and a list of acceptable passwords.
+  -- Empty list of passwords means a free access.
+  -- 
+  ServerMode_Host Int [ByteString] | 
+  -- | 
+  -- Path to the socket file.
+  -- Since sockets are local no password-protection is required.
+  -- 
+  ServerMode_Socket FilePath
+
+data Server t = Server {
+  shutdownServer :: IO ()
+}
+
+startServer :: 
+  (Tag t, Serializable IO (UnionEvent t), Serializable IO (UnionEventResult t)) =>
+  Engine t -> ServerMode -> IO (Server t)
+startServer engine serverMode = do
+  acidServer <- Server.start (void . return) (5 * 60 * 10^6) acidServerMode processRequest
+  let
+    shutdown = Server.shutdown acidServer
+  return $ Server shutdown
+  where
+    acidServerMode = case serverMode of
+      ServerMode_Socket path -> Server.Socket path
+      ServerMode_Host port passwords -> Server.Host port passwords
+    processRequest unionEvent = runUnionEvent engine unionEvent
