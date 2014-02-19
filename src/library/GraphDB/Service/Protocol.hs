995e3279f92d2ad7c865e154f498be41bb170780
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-02-19 20:44:29 +0400

    Parameterized Protocol

--- src/library/GraphDB/Service/Protocol.hs
+++ src/library/GraphDB/Service/Protocol.hs
@@ -4,50 +4,41 @@ module GraphDB.Service.Protocol where
 import GraphDB.Util.Prelude
 
 
-data Request t =
-  Request_Transaction (Request_Transaction_Spec t) Bool |
-  Request_Event (Event t)
+data Request n v t =
+  Request_Transaction (Request_Transaction_Spec n v t) |
+  Request_StartTransaction Bool |
+  Request_EndTransaction
   deriving (Generic)
 
-instance (SerializableMembers m t) => Serializable m (Request t)
+instance (Serializable m n, Serializable m v, Serializable m t) => Serializable m (Request n v t)
 
-data Request_Transaction_Spec t =
+data Request_Transaction_Spec n v t =
   Request_Transaction_Spec_GetRoot |
-  Request_Transaction_Spec_NewNode (Value t) |
-  Request_Transaction_Spec_GetTargetsByType (Ref t) (Type t)
+  Request_Transaction_Spec_NewNode v |
+  Request_Transaction_Spec_GetTargetsByType n t
   deriving (Generic)
 
-instance (SerializableMembers m t) => Serializable m (Request_Transaction_Spec t)
+instance (Serializable m n, Serializable m v, Serializable m t) => Serializable m (Request_Transaction_Spec n v t)
 
-
-data Response t =
-  Response_Transaction (Response_Transaction_Spec t) |
-  Response_Event (EventResult t)
+data Response n v =
+  Response_Transaction (Response_Transaction_Spec n v) |
+  Response_StartTransaction |
+  Response_EndTransaction
   deriving (Generic)
 
-instance (SerializableMembers m t) => Serializable m (Response t)
+instance (Serializable m n, Serializable m v) => Serializable m (Response n v)
 
-data Response_Transaction_Spec t =
-  Response_Transaction_Spec_GetRoot (Ref t) |
-  Response_Transaction_Spec_NewNode (Ref t) |
-  Response_Transaction_Spec_GetTargetsByType [Ref t]
+data Response_Transaction_Spec n v =
+  Response_Transaction_Spec_GetRoot n |
+  Response_Transaction_Spec_NewNode n |
+  Response_Transaction_Spec_GetTargetsByType [n] |
+  Response_Transaction_Spec_GetTargetsByIndex [n] |
+  Response_Transaction_Spec_AddTarget Bool |
+  Response_Transaction_Spec_RemoveTarget Bool |
+  Response_Transaction_Spec_GetValue v |
+  Response_Transaction_Spec_SetValue |
+  Response_Transaction_Spec_GetStats (Int, Int)
   deriving (Generic)
 
-instance (SerializableMembers m t) => Serializable m (Response_Transaction_Spec t)
-
-
-type family Event t
-type family EventResult t
-type family Value t
-type family Ref t
-type family Type t
-
-
-type SerializableMembers m t = 
-  (Serializable m (Event t),
-   Serializable m (EventResult t),
-   Serializable m (Value t),
-   Serializable m (Ref t),
-   Serializable m (Type t))
-
+instance (Serializable m n, Serializable m v) => Serializable m (Response_Transaction_Spec n v)
 
