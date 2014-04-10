98c939708494b213381d237858991040ebd0a547
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-04-10 20:19:43 +0400

    Cleanup StorageTests

--- htf-test-suite/HTFTestSuite/StorageTests.hs
+++ htf-test-suite/HTFTestSuite/StorageTests.hs
@@ -5,16 +5,46 @@ import Test.Framework
 import GraphDB.Util.Prelude
 import qualified GraphDB.Storage as Storage
 import qualified GraphDB.Util.FileSystem as FS
-import qualified HTFTestSuite.A as A
 
 
+
+data A = A { ref :: IORef Float }
+  deriving (Generic)
+
+instance Serializable IO A
+
+data Event = Increase | Decrease | Multiply Float | Divide Float | Get
+  deriving (Show, Ord, Eq, Generic)
+
+instance Serializable m Event
+
+data EventResult = UnitEventResult () | FloatEventResult Float 
+  deriving (Show, Ord, Eq, Generic)
+
+instance Serializable m EventResult
+
+
+initValue = A <$> newIORef 0
+
+applyEvent (A ref) e = case e of
+  Increase -> modifyIORef ref succ
+  Decrease -> modifyIORef ref pred
+  Multiply by -> modifyIORef ref (*by)
+  Divide by -> modifyIORef ref (/by)
+
+processRequestData (A ref) event = case event of
+  Increase -> modifyIORef ref succ >> readIORef ref >>= return . FloatEventResult
+  Decrease -> modifyIORef ref pred >> readIORef ref >>= return . FloatEventResult
+  Multiply by -> modifyIORef ref (*by) >> readIORef ref >>= return . FloatEventResult
+  Divide by -> modifyIORef ref (/by) >> readIORef ref >>= return . FloatEventResult
+  Get -> readIORef ref >>= return . FloatEventResult
 
 dir = "./dist/test/storage"
 
-withStorage :: ((Storage.Storage A.A A.Event, A.A) -> IO a) -> IO a
+withStorage :: ((Storage.Storage A Event, A) -> IO a) -> IO a
 withStorage = bracket acquire release 
   where
-    acquire = Storage.acquireAndLoad A.initValue A.applyEvent =<< Storage.pathsFromDirectory dir
+    acquire = Storage.acquireAndLoad initValue applyEvent =<< Storage.pathsFromDirectory dir
     release = Storage.release . fst
 
 cleanUp :: IO ()
@@ -25,83 +55,83 @@ cleanUp = FS.removeIfExists dir
 test_loadsPastTenAttempts = do 
   cleanUp
   replicateM 11 $ withStorage $ \(s, a) -> do
-    Storage.persistEvent s A.Increase
+    Storage.persistEvent s Increase
   withStorage $ \(s, a) -> do
-    assertEqual 11 =<< readIORef . A.ref =<< return a
+    assertEqual 11 =<< readIORef . ref =<< return a
 
 test_loads = do 
   cleanUp
   withStorage $ \(s, a) -> do
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s $ A.Multiply 3
-    Storage.persistEvent s A.Decrease
-    Storage.persistEvent s A.Increase
+    Storage.persistEvent s Increase
+    Storage.persistEvent s Increase
+    Storage.persistEvent s $ Multiply 3
+    Storage.persistEvent s Decrease
+    Storage.persistEvent s Increase
   withStorage $ \(s, a) -> do
-    value <- readIORef . A.ref =<< return a
+    value <- readIORef . ref =<< return a
     assertEqual 6 value
 
 test_loadsAfterMultipleRuns = do 
   cleanUp
   withStorage $ \(s, a) -> do
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s $ A.Multiply 3
-    Storage.persistEvent s A.Decrease
-    Storage.persistEvent s A.Increase
+    Storage.persistEvent s Increase
+    Storage.persistEvent s $ Multiply 3
+    Storage.persistEvent s Decrease
+    Storage.persistEvent s Increase
   withStorage $ \(s, a) -> do
-    assertEqual 3 =<< readIORef . A.ref =<< return a
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s $ A.Multiply 3
-    Storage.persistEvent s A.Decrease
-    Storage.persistEvent s A.Increase
+    assertEqual 3 =<< readIORef . ref =<< return a
+    Storage.persistEvent s Increase
+    Storage.persistEvent s Increase
+    Storage.persistEvent s $ Multiply 3
+    Storage.persistEvent s Decrease
+    Storage.persistEvent s Increase
   withStorage $ \(s, a) -> do
-    assertEqual 15 =<< readIORef . A.ref =<< return a
+    assertEqual 15 =<< readIORef . ref =<< return a
 
 test_checkpoint = do
   cleanUp
   withStorage $ \(s, a) -> do
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s $ A.Multiply 3
-    Storage.persistEvent s A.Decrease
-    Storage.persistEvent s A.Increase
+    Storage.persistEvent s Increase
+    Storage.persistEvent s $ Multiply 3
+    Storage.persistEvent s Decrease
+    Storage.persistEvent s Increase
   withStorage $ \(s, a) -> do
-    Storage.checkpoint s =<< A.A <$> newIORef 3
-  withStorage $ \(s, a) -> assertEqual 3 =<< readIORef . A.ref =<< return a
+    Storage.checkpoint s =<< A <$> newIORef 3
+  withStorage $ \(s, a) -> assertEqual 3 =<< readIORef . ref =<< return a
   withStorage $ \(s, a) -> do
-    Storage.checkpoint s =<< A.A <$> newIORef 3
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s $ A.Multiply 3
-    Storage.persistEvent s A.Decrease
-    Storage.persistEvent s A.Increase
-  withStorage $ \(s, a) -> assertEqual 15 =<< readIORef . A.ref =<< return a
+    Storage.checkpoint s =<< A <$> newIORef 3
+    Storage.persistEvent s Increase
+    Storage.persistEvent s Increase
+    Storage.persistEvent s $ Multiply 3
+    Storage.persistEvent s Decrease
+    Storage.persistEvent s Increase
+  withStorage $ \(s, a) -> assertEqual 15 =<< readIORef . ref =<< return a
 
 test_checkpointWithoutEvents = do 
   cleanUp
   replicateM 11 $ withStorage $ \(s, a) -> do
-    A.applyEvent a A.Increase
-    A.applyEvent a A.Increase
+    applyEvent a Increase
+    applyEvent a Increase
     Storage.checkpoint s a
   withStorage $ \(s, a) -> do
-    assertEqual 22 =<< readIORef . A.ref =<< return a
+    assertEqual 22 =<< readIORef . ref =<< return a
 
 test_checkpointWithEvents = do 
   cleanUp
   replicateM 11 $ withStorage $ \(s, a) -> do
-    Storage.persistEvent s A.Increase
-    Storage.persistEvent s A.Increase
-    A.applyEvent a A.Increase
-    A.applyEvent a A.Increase
+    Storage.persistEvent s Increase
+    Storage.persistEvent s Increase
+    applyEvent a Increase
+    applyEvent a Increase
     Storage.checkpoint s a
   withStorage $ \(s, a) -> do
-    assertEqual 22 =<< readIORef . A.ref =<< return a
+    assertEqual 22 =<< readIORef . ref =<< return a
 
 test_cleanUp = do
   cleanUp
   withStorage $ \(s, a) -> return ()
-  withStorage $ \(s, a) -> Storage.checkpoint s =<< A.A <$> newIORef 3
-  withStorage $ \(s, a) -> Storage.checkpoint s =<< A.A <$> newIORef 3
+  withStorage $ \(s, a) -> Storage.checkpoint s =<< A <$> newIORef 3
+  withStorage $ \(s, a) -> Storage.checkpoint s =<< A <$> newIORef 3
   withStorage $ \(s, a) -> return ()
   assertBool =<< FS.getExists (dir <> "4.checkpoint")
   assertBool =<< FS.getExists (dir <> "5.events")
