bee86cde94447cf57ac4ba267fa9ac927c7fb046
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-04-25 20:46:41 +0400

    Nonpersistent benchmark

---
+++ benchmarks/Nonpersistent.hs
@@ -0,0 +1,25 @@
+
+import Benchmarks.Prelude
+import qualified CriterionPlus as Cri
+import qualified Benchmarks.GraphDB as Gra
+import qualified Benchmarks.Random as Ran
+import qualified Benchmarks.Model as Mod
+
+
+main = do
+  gen <- Ran.newGen
+  Cri.benchmark $ do
+    Cri.standoff "Inserting" $ do
+      let subject n = do
+            session <- Ran.runGenT gen $ do
+              list <- replicateM n $ Ran.generateName
+              return $ forM_ list $ Mod.insertArtist . Mod.Artist
+            Cri.subject (cs $ show n) $ do
+              Cri.pause
+              Gra.runNonpersistentSession $ do
+                lift $ Cri.continue
+                Gra.interpretSession session
+                lift $ Cri.pause
+      subject 11000
+      subject 6000
+      subject 1000
