df6945bce7488fa48be8ab5899d3d981d3f418f1
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-04-29 03:34:29 +0400

    restore THTests

--- internal-tests/Main.hs
+++ internal-tests/Main.hs
@@ -5,6 +5,7 @@ import GraphDB.Util.Prelude
 
 import {-@ HTF_TESTS @-} InternalTests.StorageTests
 import {-@ HTF_TESTS @-} InternalTests.GraphTests
+import {-@ HTF_TESTS @-} InternalTests.THTests
 
 
 main = htfMain $ htf_thisModulesTests : htf_importedTests
